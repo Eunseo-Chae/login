5fd377bb119e72bdf44f536d136885b3b607882a
ChaeEunseo <20212034@edu.hanbat.ac.kr>
2022-09-09 22:34:12 +0900

    new

--- LoginApplication.java
+++ LoginApplication.java
@@ -16,7 +16,7 @@ public class LoginApplication {
             if (selectNo == 1) {
                 createAccount();
             } else if (selectNo == 2) {
-                login_login();
+                doLogin();
             } else if (selectNo == 3) {
                 accountList();
             } else if (selectNo == 4) {
@@ -37,7 +37,7 @@ public class LoginApplication {
         System.out.print("nickname: ");
         String nickname = scanner.next();
 
-        if (test(id)) {
+        if (accountCheck(id)) {
             System.out.println("회원가입이 안됩니다.");
             return;
         }
@@ -52,7 +52,7 @@ public class LoginApplication {
             }
         }
     }
-    private static boolean test(String id) {
+    private static boolean accountCheck(String id) {
         for (User value : accountArray) {
             if (value != null && value.getId().equals(id)) {
                 return true;
@@ -63,31 +63,31 @@ public class LoginApplication {
 
     private static void accountList() {     //3번 사용자 조회
         System.out.println("사용자 조회");
-        for (User loginbox : accountArray) {
-            if (loginbox != null) {
-                System.out.println("nickname:" + loginbox.getNickname());
+        for (User login_box : accountArray) {
+            if (login_box != null) {
+                System.out.println("nickname:" + login_box.getNickname());
             }
         }
     }
-    private static void login_login() {    //2번 로그인하기
+    private static void doLogin() {    //2번 로그인하기
         System.out.println("로그인 하세요");
         System.out.print("id:");
         String id = scanner.next();
         System.out.print("password:");
         String password = scanner.next();
 
-        User loginbox = findLogin(id, password);
-        if(loginbox==null){
+        User login_box = findLogin(id, password);
+        if(login_box==null){
             System.out.println("로그인 아이디와 패스워드가 일치하지 않습니다.");
             return;
         }
-        loginbox.setid(loginbox.getId());
-        loginbox.setPassword(loginbox.getPassword());
-        loginbox.setNickname(loginbox.getNickname());
+        login_box.setid(login_box.getId());
+        login_box.setPassword(login_box.getPassword());
+        login_box.setNickname(login_box.getNickname());
 
         System.out.println("로그인이 되었습니다.");
-        System.out.println("id :" + loginbox.getId());
-        System.out.println("nickname :" + loginbox.getNickname());
+        System.out.println("id :" + login_box.getId());
+        System.out.println("nickname :" + login_box.getNickname());
     }
     private static void withdraw(){   //회원탈퇴
         System.out.println("회원탈퇴창 입니다.");
@@ -96,36 +96,35 @@ public class LoginApplication {
         System.out.print("password:");
         String password = scanner.next();
 
-        User loginbox = findLogin(id, password);
+        User login_box = findLogin(id, password);
 
-
-
-
-
-//        if(loginbox == null){
-//            System.out.println("회원탈퇴를 할 수 없습니다.");
-//            return;
-//        }else {
-//            loginbox.setid(null);
-//            loginbox.setPassword(null);
-//            loginbox.setNickname(null);
-//            System.out.println("회원탈퇴 되었습니다.");
-//        }
+        if(login_box == null){
+            System.out.println("회원탈퇴를 할 수 없습니다.");
+            return;
+        }else {
+            for (int i=0; i<accountArray.length; i++){
+                if((id).equals(accountArray[i].getId())&&(password).equals(accountArray[i].getPassword())){
+                    accountArray[i]=null;
+                    break;
+                }
+            }
+            System.out.println("회원탈퇴 되었습니다.");
+      }
     }
     private static User findLogin(String id, String password){
-        User loginbox = null;
+        User login_box = null;
         for (int i=0; i< accountArray.length; i++){
             if(accountArray[i]!=null){
-                String dbid = accountArray[i].getId();
-                String dbpassword = accountArray[i].getPassword();
+                String idBox = accountArray[i].getId();
+                String passwordBox = accountArray[i].getPassword();
 
-                if(dbid.equals(id)&&dbpassword.equals(password)){
-                    loginbox = accountArray[i];
+                if(idBox.equals(id)&&passwordBox.equals(password)){
+                    login_box = accountArray[i];
                     break;
                 }
             }
         }
-        return loginbox;
+        return login_box;
     }
 
 }
